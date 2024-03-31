fd5e8a67c33032e5c29801f3a4ea24524f6b937e
nandajfa <nanda.jfa@gmail.com>
2024-03-31 20:33:02 -0300

    adicionando services para crud de serviços

---
+++ backend/services/services.js
@@ -0,0 +1,40 @@
+const {createService, getAllServices, updateService, deleteService} = require('../db/supabase')
+
+exports.getServices = async () => {
+    try {
+        const data = await getAllServices()
+        return(data)
+    } catch (error){
+        console.error('Erro ao buscar serviços:', error.message);
+        throw new Error('Erro interno do servidor.')
+    }
+}
+
+exports.addService = async (serviceData) => {
+    try {
+        await createService(serviceData)
+        return { message: 'Serviço adicionado com sucesso.' }
+    } catch (error) {
+        console.error('Erro ao adicionar serviço:', error.message);
+        throw new Error('Erro interno do servidor.')
+    }
+}
+
+exports.editServiceByID = async (id, serviceData) => {
+    try {
+        const result = await updateService(id, serviceData);
+        return result;
+    } catch (error) {
+        console.error('Erro ao editar serviço:', error.message);
+        throw new Error('Erro interno do servidor.')
+    }
+}
+
+exports.deleteServiceByID = async (id) => {
+    try {
+        await deleteService(id);
+    } catch (error) {
+        console.error('Erro ao deletar serviço:', error.message);
+        throw new Error('Erro interno do servidor.')
+    }
+}
