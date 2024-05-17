f46d798c88ae048eef98d9e35c4a70e272500e03
nandajfa <nanda.jfa@gmail.com>
2024-05-16 21:38:36 -0300

    :sparkles: new route - dataUser and logout

--- backend/routes/Routes.js
+++ backend/routes/Routes.js
@@ -1,14 +1,29 @@
 const express = require('express')
-const router = express.Router();
-const { registerUserController, loginUserController} = require('../controllers/authController');
-const { getService, addService, editService, deleteService } = require('../controllers/serviceController')
+const router = express.Router()
+const {
+  registerUserController,
+  loginUserController,
+  dataUserController,
+  logoutController,
+  dataUserController
+} = require('../controllers/authController')
+const {
+  getService,
+  addService,
+  editService,
+  deleteService
+} = require('../controllers/serviceController')
+const cookieParser = require('cookie-parser')
+router.use(cookieParser())
 
-router.post('/register', registerUserController);
-router.post('/login', loginUserController);
+router.post('/register', registerUserController)
+router.post('/login', loginUserController)
+router.get('/dataUser', dataUserController)
+router.get('/logout', logoutController)
 
-router.get('/', getService);
-router.post('/', addService);
-router.put('/:id', editService);
-router.delete('/:id', deleteService);
+router.get('/', getService)
+router.post('/', addService)
+router.put('/:id', editService)
+router.delete('/:id', deleteService)
 
-module.exports = router;
+module.exports = router
