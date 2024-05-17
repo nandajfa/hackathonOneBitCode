394e0124f0c22c4241e8d8989dfeb83f6d67a598
nandajfa <nanda.jfa@gmail.com>
2024-05-16 21:39:59 -0300

    :sparkles: dataUser controller and service

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,23 +1,48 @@
-const { registerUser, loginUser } = require('../services/authServices');
+const {
+  registerUser,
+  loginUser,
+  dataUser,
+  logoutUser
+} = require('../services/authServices')
 
 exports.registerUserController = async (req, res, next) => {
   try {
-    const { name, email, password } = req.body;
-    const result = await registerUser(name, email, password);
-    res.status(201).json(result);
+    const { name, email, password } = req.body
+    const result = await registerUser(name, email, password)
+    res.status(201).json(result)
   } catch (error) {
-    console.error('Erro ao registrar usuário:', error.message);
-    next(error);
+    console.error('Erro ao registrar usuário:', error.message)
+    next(error)
   }
-};
+}
 
 exports.loginUserController = async (req, res, next) => {
   try {
-    const { email, password } = req.body;
-    const result = await loginUser(email, password);
-    res.json(result);
+    const { email, password } = req.body
+    const result = await loginUser(req, res, email, password)
+    res.json(result)
   } catch (error) {
-    console.error('Erro ao fazer login:', error.message);
-    next(error);
+    console.error('Erro ao fazer login:', error.message)
+    next(error)
   }
-};
+}
+
+exports.dataUserController = async (req, res, next) => {
+  try {
+    const result = await dataUser(req, res)
+    res.json(result)
+  } catch (error) {
+    console.error('Erro ao acessar perfil:', error.message)
+    next(error)
+  }
+}
+
+exports.logoutController = async (req, res, next) => {
+  try {
+    const result = await logoutUser(req, res)
+    res.json(result)
+  } catch (error) {
+    console.error('Erro ao fazer logout:', error.message)
+    next(error)
+  }
+}
--- backend/services/authServices.js
+++ backend/services/authServices.js
@@ -1,10 +1,13 @@
-const { createUser, findUserByEmail } = require('../db/supabase')
+const { createUser, findUserByEmail, findUserById } = require('../db/supabase')
 const {
   generateToken,
   generateRandomString,
   hashPassword,
   comparePasswords
 } = require('../utils/authUtils')
+// const cookieParser = require('cookie-parser')
+const jwt = require('jsonwebtoken')
+const secretManager = require('../utils/secretManager')
 
 exports.registerUser = async (name, email, password) => {
   try {
@@ -23,7 +26,7 @@ exports.registerUser = async (name, email, password) => {
   }
 }
 
-exports.loginUser = async (email, password) => {
+exports.loginUser = async (req, res, email, password) => {
   try {
     const user = await findUserByEmail(email)
     if (!user) {
@@ -36,11 +39,46 @@ exports.loginUser = async (email, password) => {
     }
 
     const secret = generateRandomString(32)
+    await secretManager.setSecret(secret)
     const token = await generateToken({ userId: user.id }, secret, '24h')
 
-    return { token }
+    if (!token) {
+      return 'Fail'
+    } else {
+      res.cookie('token', token, { httpOnly: true, secure: true })
+      return 'Success'
+    }
   } catch (error) {
     console.error('Erro ao fazer login:', error.message)
     throw new Error('Usuário não encontrado')
   }
+}
+
+exports.dataUser = async (req, res) => {
+  const token = req.cookies.token
+
+  if (token) {
+    try {
+      const secret = await secretManager.getSecret()
+      const decodedToken = jwt.verify(token, secret)
+
+      let data = await findUserById(decodedToken.userId)
+
+      return data
+    } catch (error) {
+      console.error('Erro ao verificar o token:', error.message)
+      res.sendStatus(401)
+    }
+  } else {
+    res.sendStatus(401)
+  }
+}
+
+exports.logoutUser = (req, res) => {
+  res.cookie('token', '', {
+    expires: new Date(0),
+    httpOnly: true,
+    secure: true
+  })
+  res.send('Token de autenticação removido com sucesso.')
 }
