06c72d00587a175b1c493e3e42765c21ecc9ccc0
nandajfa <nanda.jfa@gmail.com>
2024-05-20 16:07:48 -0300

    :sparkles: add function to get/set secret

--- backend/utils/secretManager.js
+++ backend/utils/secretManager.js
@@ -2,10 +2,8 @@ let secret
 
 exports.setSecret = newSecret => {
   secret = newSecret
-  console.log('new:', secret)
 }
 
 exports.getSecret = () => {
-  console.log('let scret: ', secret)
   return secret
 }
